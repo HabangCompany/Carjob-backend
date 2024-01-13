7aaafd00d2af4bedd69bea1b31f3e0304d524e3d
하방방 <81242485+jumosd@users.noreply.github.com>
2024-01-13 13:48:21 +0900

    CORS 해결방법

--- carjab/carjab/settings.py
+++ carjab/carjab/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'account',
-
-
 ]
 
 MIDDLEWARE = [
@@ -52,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+#CORS 허용 도메인
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # React 개발 서버의 주소
 ]
 
 ROOT_URLCONF = 'carjab.urls'
