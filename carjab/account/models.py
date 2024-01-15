6f1fafcb9f467c8bd80525ba6a120f335e761d85
하방방 <81242485+jumosd@users.noreply.github.com>
2024-01-15 11:57:44 +0900

    maria db연결 회원가입 모델 만드는중

--- carjab/account/models.py
+++ carjab/account/models.py
@@ -4,13 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 #일반 회원
 class User(AbstractUser):
+    nickname = models.CharField(max_length =10,unique = True, null=False)
     address = models.CharField(max_length = 50)
-    phonenumber = models.CharField(max_length = 16, null=True)
+    phonenumber = models.CharField(max_length = 20, null=True)
 
+    class Meta:
+        db_table = "User"
     def __str__(self):
-        """A string representation of the model."""
         return self.username
 
 #업체회원
-class Store(User):
-    pass
+class Store(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    storeName = models.CharField(max_length =20, blank =False , unique=True)
+    class Meta:
+        db_table ="Store"
+    
+    def __str__(self):
+        return self.storeName
--- carjab/account/views.py
+++ carjab/account/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def login(request):
     return HttpResponse("로그인")
@@ -11,12 +12,8 @@ def login(request):
 
 @api_view(['POST'])
 def signup(request):
-    if request.method  == "POST":
-        print("회원가입요청")
-        print(request.data["data"])
-        print(request)
-        data = {
-            'message':"회원가입완료"
-        }
+    data = {
+        'message':"회원가입완료"
+    }
 
-        return Response()
+    return Response(data=data)
